b6ab264d160cce5fdbfa88bb8798996f5a5496c4
Auryx <131199997+Auryx@users.noreply.github.com>
2023-04-27 18:49:37 -0700

    Add files via upload

--- terminal_homework.bash
+++ terminal_homework.bash
@@ -3,56 +3,92 @@
 # Open the **Terminal app**
 
 # Create a new directory on your desktop called `galaxy_far_far_away`and enter it
+$ mkdir galaxy_far_far_away && cd galaxy_far_far_away
 
 # Create a directory called `death_star`
+
 
 # and make the following files inside of it:
 # darth_vader.txt
 # princess_leia.txt
 # storm_trooper.txt
+$ mkdir death_star && cd death_star && touch darth_vader.txt && touch princess_leia.txt && touch storm_trooper.txt
 
 # In `galaxy_far_far_away`, make a directory named `tatooine`
+cd ..
+mkdir tatooine && cd tatooine
 
 # and create the following files in it:
 # luke.txt
 # ben_kenobi.txt
+$ touch luke.txt && touch ben_kenobi.txt
 
 # Inside of `tatooine` make a directory called `millenium_falcon`
 # and in it create:
 # han_solo.txt
 # chewbaca.txt
+$ mkdir millenium_falcon && cd millenium_falcon
+$ touch han_solo.txt && touch chewbaca.txt
 
 # Rename `ben_kenobi.txt` to `obi_wan.txt
+cd.. 
+mv ben_kenobi.txt obi_wan.txt
 
 # Copy `storm_trooper.txt` from `death_star` to `tatooine`
+cd ..
+cd death_star 
+cp storm_trooper.txt ~/Seir-Kale/Unit1/Week1/first_homework/galaxy_far_far_away/tatooine/
 
 # Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
+mv luke.txt obi_wan.txt millenium_falcon/ 
 
 # Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
-
+mv millenium_falcon .. 
 # Move `millenium_falcon` into `death_star`
-
+mv millenium_falcon death_star/
 # Move `princess_leia.txt` into the `millenium_falcon`
+cd death_star
+mv princess_leia.txt millenium_falcon/
 
 # Delete `obi_wan.txt`
+cd millenium_falcon
+rm -rf obi_wan.txt
 
 # In `galaxy_far_far_away`, make a directory called `yavin_4`
+cd ..
+mkdir yavin_4
 
 # Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
+mv millenium_falcon ..
+cd ..
+mv millenium_falcon yavin_4/
 
 # Make a directory in `yavin_4` called `x_wing`
+mkdir x_wing
 
 # Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
+cd millenium_falcon
+mv luke.txt princess_leia.txt ..
+cd ..
+mv luke.txt princess_leia.txt x_wing/
 
 # Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
+mv millenium_falcon x_wing ..
 
 # In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+cd ..
+cd death_star
+mkdir tie_fighter_1 tie_fighter_2 tie_fighter_3
 
 # Move `darth_vader.txt` into `tie_fighter_1`
+mv darth_vader.txt tie_fighter_1
 
 # Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
+cp storm_trooper.txt tie_fighter_2
+cp storm_trooper.txt tie_fighter_3
 
 # Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
+mv tie_fighter_1 tie_fighter_2 tie_fighter_3 ..
 
 # Be careful with this command - cannot undo!
 
@@ -61,11 +97,18 @@
 # This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
 
 # Remove `tie_fighters` 2 and 3.
+cd ..
+rm -rf tie_fighter_2 tie_fighter_3
 
 # Touch a file in "**x_wing**" called "**the_force.txt**".
+cd x_wing/
+touch the_force.txt
 
 # Destroy the "**death_star**" and anyone inside of it.
+cd ..
+rm -rf death_star/
 
 # Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+mv x_wing millenium_falcon yavin_4/
 
 # Celebrate!
